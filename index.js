624debaff58a26ea181de0d6956948e8640c5f14
Prafulla Mishra <prafullamishra45@gmail.com>
2026-01-05 05:16:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+//Routes
+
+//Home route
+app.listen(8000,()=>{
+    console.log(`Server is running on port ${port}`);
+})
