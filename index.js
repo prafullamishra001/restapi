a0d17efec37eb63fe49e638e333731a1956be594
Prafulla Mishra <prafullamishra45@gmail.com>
2026-01-05 06:00:07 +0530

    individual user done

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.get('/api/user',(req,res)=>{
    return res.json(users);
 });
 
+app.get('/api/user/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const u=users.find(u=>u.id==id);
+    return res.json(u);
+})
+
 
 app.listen(8000,()=>{
     console.log(`Server is running on port ${port}`);
